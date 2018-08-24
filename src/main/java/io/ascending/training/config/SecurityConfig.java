d7cb071665e35150d5c90462c982b87701c1fb9c
ryo <ryo.hang@gmail.com>
2018-08-24 16:05:01 -0400

    add security config

--- src/main/java/io/ascending/training/config/SecurityConfig.java
+++ src/main/java/io/ascending/training/config/SecurityConfig.java
@@ -78,4 +78,61 @@ public class SecurityConfig {
         }
     }
 
+
+//    @Configuration
+//    @Order(1)
+//    public static class RestWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//        @Autowired
+//        private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+//        //curl -i -X POST -d username=ryohang1 -d password=password123 -c ./cookies.txt http://localhost:8080/login
+//        //curl --cookie ./cookies.txt http://localhost:8080/api/cars
+//        @Autowired
+//        private UserDetailsService userDetailsService;
+////        @Autowired
+////        private JwtAuthenticationFilter jwtAuthenticationFilter;
+//
+//        @Autowired
+//        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//            PasswordEncoder encoder = new BCryptPasswordEncoder();
+//            auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+//        }
+//
+//        @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+//        @Override
+//        public AuthenticationManager authenticationManagerBean() throws Exception {
+//            return super.authenticationManagerBean();
+//        }
+//
+//        @Override
+//        public void configure(WebSecurity web) throws Exception {
+//            web.ignoring()
+//                    .antMatchers("/resources/**");
+//        }
+//        protected void configure(HttpSecurity http) throws Exception {
+//            //http://www.baeldung.com/securing-a-restful-web-service-with-spring-security
+//            http //.addFilterAt(new AnonymousAuthenticationFilter("ascending_key") ,AnonymousAuthenticationFilter.class)
+////                  .addFilterAt(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class)
+//                    .csrf().disable().authorizeRequests().antMatchers("/api/users/login","/api/users/signup").permitAll()
+//                    .and()
+//                    //enable curl
+//                    //.authorizeRequests().antMatchers("/api/**").authenticated()
+//                    .authorizeRequests().antMatchers("/api/**").hasAnyRole("REGISTERED_USER","ADMIN")
+//                    .and()
+//                    .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
+//                    .and()
+////                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////                .and()
+//                    .formLogin();
+//        }
+//    }
+
+//    @Configuration
+//    public static class ResourcesWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+//        @Override
+//        public void configure(WebSecurity web) throws Exception {
+//            web.ignoring()
+//                    .antMatchers("/resources/**","/swagger-ui.html","/webjars/**");
+//        }
+//    }
+
 }
