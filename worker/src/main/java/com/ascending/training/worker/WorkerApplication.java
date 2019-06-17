3a221590e7d6925b479a35ce79e396d852261bd4
ryo <ryo.hang@gmail.com>
2019-06-17 11:18:06 -0400

    update sqs integration

--- worker/src/main/java/com/ascending/training/worker/WorkerApplication.java
+++ worker/src/main/java/com/ascending/training/worker/WorkerApplication.java
@@ -2,14 +2,13 @@ package com.ascending.training.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WorkerApplication {
 
     public static void main(String[] args) {
-        ApplicationContext app =SpringApplication.run(WorkerApplication.class, args);
-//        SQSMessageService messageService = app.getBean(SQSMessageService.class);
-//        messageService.receiveMessage();
+        SpringApplication.run(WorkerApplication.class, args);
+//      SQSMessageService messageService = app.getBean(SQSMessageService.class);
+//      messageService.receiveMessage();
     }
 }
--- worker/src/main/java/com/ascending/training/worker/service/ProcessService.java
+++ worker/src/main/java/com/ascending/training/worker/service/ProcessService.java
@@ -13,18 +13,19 @@ import java.util.Map;
 
 @Component
 public class ProcessService {
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     @JmsListener(destination = "car-demo-dev")
     public void processMessage(String msg) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = new HashMap<>();
-        // convert JSON string to Map
-        map = mapper.readValue(msg, new TypeReference<Map<String, String>>(){});
-        String msgType = (String)map.get("id");
-        String msgText = (String)map.get("domainName");
-        Long userID = Long.valueOf(msgText);
-        logger.info("receive msgType: "+msgType);
-        logger.info("receive msgText: "+msgText);
+        System.out.println(msg);
+//        ObjectMapper mapper = new ObjectMapper();
+//        Map<String, Object> map = new HashMap<>();
+//        // convert JSON string to Map
+//        map = mapper.readValue(msg, new TypeReference<Map<String, String>>(){});
+//        String msgType = (String)map.get("id");
+//        String msgText = (String)map.get("domainName");
+//        Long userID = Long.valueOf(msgText);
+//        logger.info("receive msgType: "+msgType);
+//        logger.info("receive msgText: "+msgText);
     }
 
 }
